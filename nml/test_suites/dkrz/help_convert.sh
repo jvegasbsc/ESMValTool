11538bb235906207a349f8fd77f2d02cdd7f5df9
Lisa Bock <Lisa.Bock@dlr.de>
2017-10-20 11:49:53 +0200

    one help_convert-sh for all user

--- nml/test_suites/dkrz/help_convert.sh
+++ nml/test_suites/dkrz/help_convert.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-for ip2f in ../../namelist*xml
+#for ip2f in ../../test_suites/easytest/namelist_collins*xml
+for ip2f in ../../namelist_GlobalOcean*xml
 do
     echo $ip2f
     ifile=$(echo ${ip2f}|cut -d'/' -f3)
     cp ${ip2f} ./${ifile}
-    python /pf/b/b309070/util/translate2ESGF/translate2ESGF.py -n ${ifile}
+    python /pf/b/$USER/util/translate2ESGF/translate2ESGF.py -n ${ifile}
     touch test.xml
     mv test.xml ${ifile}
 done
