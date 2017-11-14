e5770acc5274c7a66a3ec2b43ed17859cc9d26b1
Manuel Schlund <manuel.schlund@dlr.de>
2017-11-14 14:46:26 +0100

    Modified format of class description

--- interface_scripts/constants.py
+++ interface_scripts/constants.py
@@ -2,23 +2,22 @@
 ###############################################################################
 GENERAL COLLECTION OF PHYSICAL CONSTANTS
 ###############################################################################
-
-Description
-    Collection of important physical constants. The constants are stored as
-    static members of the class "Constant" to avoid the use of global
-    variables.
-
-Contents
-    Earth radius [m]
-
-Source
-    https://physics.nist.gov/cuu/Constants/Table/allascii.txt
-
-###############################################################################
 """
 
 
 class Constant(object):
+    """
+    Description
+        Collection of important physical constants. The constants are stored as
+        static members of the class "Constant" to avoid the use of global
+        variables.
+
+    Contents
+        Earth radius [m]
+
+    Source
+        https://physics.nist.gov/cuu/Constants/Table/allascii.txt
+    """
 
     # Earth radius [m]
     Earth_radius = 6371000.0
