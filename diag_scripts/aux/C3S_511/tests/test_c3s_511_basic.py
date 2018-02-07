5c84354edc5fd3f8ad81fb355b69bc2e9c123072
Björn Brötz <bjoern.broetz@dlr.de>
2018-02-07 17:06:51 +0100

    Add tests

--- diag_scripts/aux/C3S_511/tests/test_c3s_511_basic.py
+++ diag_scripts/aux/C3S_511/tests/test_c3s_511_basic.py
@@ -7,9 +7,19 @@ from c3s_511_basic import __Diagnostic_skeleton__, Basic_Diagnostic
 
 class TestDiagnosticSkeleton:
     def setup(self):
-        pass
+        self.S = __Diagnostic_skeleton__()
     def test_init(self):
-        assert False
+        self.S.__init__()
+        assert isinstance(self.S.__project_info__, dict)
+        assert os.path.isdir(self.S.__plot_dir__)
+        assert os.path.isdir(self.S.__work_dir__)
+        assert self.S.__varname__ == "var"
+        assert self.S.__output_type__ == "png"
+        assert self.S.__regions__ == {"example":(10,20,-10,-20)}
+        assert self.S.__verbosity_level__ == 0
+        assert self.S.__debug_info__ == "No debug info"
+        assert isinstance(self.S.__config__, dict) 
+        
     def test_read_data(self):
         assert False
     def test_run_diagnostic(self):
