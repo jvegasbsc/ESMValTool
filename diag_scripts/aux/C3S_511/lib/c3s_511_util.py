f61dd379585c22c2b7226caa36bfc04db3871d38
BenMGeo <b.mueller@iggf.geo.uni-muenchen.de>
2018-02-07 12:36:44 +0100

    restructure subfuntions in c3s_511

--- diag_scripts/aux/C3S_511/lib/c3s_511_util.py
+++ diag_scripts/aux/C3S_511/lib/c3s_511_util.py
@@ -5,3 +5,14 @@ Created on Wed Feb  7 12:17:48 2018
 
 """
 
+import imp
+
+def __getInfoFromFile__(filename):
+    """
+    routine to read cfg file
+    """
+    f = open(filename)
+    __config__ = imp.load_source('cfg', '', f)
+    f.close()
+    return __config__
+        
