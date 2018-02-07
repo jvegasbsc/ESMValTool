089885b448c68c70f729b9349266c0196e6cf77f
Björn Brötz <bjoern.broetz@dlr.de>
2018-02-07 12:21:26 +0100

    Add tests for c3s_511_basic

---
+++ diag_scripts/aux/C3S_511/tests/test_c3s_511_basic.py
@@ -0,0 +1,38 @@
+import pytest
+import os
+import sys
+sys.path.insert(0, os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),'..'))
+from c3s_511_basic import Diagnostic_skeleton, Basic_Diagnostic
+
+class TestDiagnosticSkeleton:
+    def setup(self):
+        assert False
+    def test_init(self):
+        assert False
+    def test_read_data(self):
+        assert False
+    def test_run_diagnostic(self):
+        assert False
+    def test_file_check(self):
+        assert False
+    def test_do_overview(self):
+        assert False
+    def test_do_mean_var(self):
+        assert False
+    def test_do_trends(self):
+        assert False
+    def test_do_extremes(self):
+        assert False
+    def test_do_matureity_matrix(self):
+        assert False
+    def test_do_prepare_report(self):
+        assert False
+
+
+class TestDiagnosticSkeleton:
+    def setup(self):
+        assert False
+    def test_init(self):
+        assert False
+
