3c6a7e49ede9ac551c603a778c7b154dac43523a
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2018-08-14 16:23:13 +0200

    Update cfg_C3S_511.py

--- nml/cfg_C3S_511/cfg_C3S_511.py
+++ nml/cfg_C3S_511/cfg_C3S_511.py
@@ -1,13 +1,13 @@
 # This is a dummy config file for C3S_511 diagnostics
 
 # 3D-data levels
-levels=[80000,25000]
+levels=[30000,50000]
 
 # color setting for data
-data_colors="cool"
+data_colors="binary"
 
 # latex output 
 # (options: True = Latex output, False = No Latex_outputs, None = No Pdf generation)
-show_latex=None
+show_latex=True
 
 
