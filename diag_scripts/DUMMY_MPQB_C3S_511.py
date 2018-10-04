d6d5b94a24844acea2f45c30ebd909f3e46f26a2
BenMGeo <b.mueller@iggf.geo.uni-muenchen.de>
2018-10-04 17:22:33 +0200

    Switched structure to MPQB and SPQB

---
+++ diag_scripts/DUMMY_MPQB_C3S_511.py
@@ -0,0 +1,72 @@
+"""
+;;#############################################################################
+;; C3S_511 MPQB Diagnostics
+;; Author: Benjamin Mueller (LMU Munich, GER)
+;; C3S_511 service
+;;#############################################################################
+;; Description
+;;    Produces various general diagnostic plots, statistics and reports for the
+;;    C3S_511 service
+;;
+;; Required diag_script_info attributes (diagnostics specific)
+;;    none
+;;
+;; Optional diag_script_info attributes (diagnostic specific)
+;;    none
+;;
+;; Required variable_info attributes (variable specific)
+;;    none
+;;
+;; Optional variable_info attributes (variable specific)
+;;    none
+;;
+;; Caveats
+;;
+;; Modification history
+;;    20181003-A_muel_bn: Routines written.
+;;
+;;#############################################################################
+"""
+
+# Basic Python packages
+import sys
+from copy import copy
+
+# Add subfolder of the diagnostics to the path
+sys.path.append('./diag_scripts/aux/C3S_511/')
+
+from esmval_lib import ESMValProject
+
+# Import full diagnostic routine
+from c3s_511_basic import Basic_Diagnostic_Multiproduct
+
+def main(project_info):
+    print('>>>>>>>> DUMMY_MPQB_C3S_511.py is running! <<<<<<<<<<<<')
+
+# A_laue_ax+
+    E = ESMValProject(project_info)
+
+    verbosity = E.get_verbosity()
+    diag_script = E.get_diag_script_name()
+
+# TODO Enter C3S_511 references
+#    E.write_references(diag_script,              # diag script name
+#                       ["A_muel_bn"],            # authors
+#                       [""],                     # contributors
+#                       [""],                     # diag_references
+#                       ["E_qa4ecv_albedo"],      # obs_references
+#                       ["P_qa4ecv"],             # proj_references
+#                       project_info,
+#                       verbosity,
+#                       False)
+# A_laue_ax-
+
+    Diag = Basic_Diagnostic_Multiproduct()
+    
+    Diag.set_info(proj_info=E)
+    Diag.read_data()
+    Diag.run_diagnostic()
+
+   
+    print('>>>>>>>> ENDED SUCESSFULLY!! <<<<<<<<<<<<')
+    print('')
