b9ce047a12edf776c3f20753ca8831fcf3b26380
BenMGeo <b.mueller@iggf.geo.uni-muenchen.de>
2019-07-18 14:47:07 +0200

    first full functional draft of extremes framework   - @bascrezee for testing needed   - table output currently in log file   - plots as simple as possible

--- esmvaltool/diag_scripts/qualassess/auxiliary/c3s_511_ex.py
+++ esmvaltool/diag_scripts/qualassess/auxiliary/c3s_511_ex.py
@@ -12,6 +12,8 @@ import sys
 import matplotlib.pyplot as plt
 import datetime
 import numpy as np
+import cf_units
+from scipy import sin, cos, tan, arctan, arctan2, arccos, pi, deg2rad
 
 from .c3s_511_basic import Basic_Diagnostic_SP
 from .libs.MD_old.ESMValMD import ESMValMD
@@ -76,16 +78,18 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
         # we assume the regions to be spatio-temporally buffered.
         list_of_plots = []
         
-        for r in self.__regions__:
-            spat_r = self.__regions__[r]
-            loc_cube = self.__spatiotemp_subsets__(cube,{r:spat_r})
+        for r,def_r in self.__regions__.items():
+            loc_cube = self.__spatiotemp_subsets__(cube,{r:def_r})
             
             list_of_cubes = []
             
-            for (entry,cube) in loc_cube.items():
+            for (entry,ecube) in loc_cube.items():
                 for rcoord in ["day_of_month", "month_number", "year"]:
-                    if rcoord in [coord.name() for coord in cube.coords()]:
-                        cube.remove_coord(rcoord)
+                    if rcoord in [coord.name() for coord in ecube.coords()]:
+                        ecube.remove_coord(rcoord)
+                        
+            latbnds = ecube.coord("latitude").bounds
+            lonbnds = ecube.coord("longitude").bounds
             
             for yx_slice in loc_cube[r].slices(['time']):
                 agg_cube = yx_slice.aggregated_by("day_of_year",iris.analysis.MEAN) 
@@ -108,7 +112,10 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
                                 iris.Constraint(coord_values={'day_of_year':
                                     lambda cell: tmin <= cell <= tmax}))
                     try:
-                        perc = doy_sel.collapsed("time",iris.analysis.PERCENTILE,percent=[which_percentile]).core_data()
+                        perc = doy_sel.collapsed("time",
+                                                 iris.analysis.PERCENTILE,
+                                                 percent=[which_percentile]
+                                                 ).core_data()
                     except:
                         perc = doy_sel.core_data()
                     if np.ma.is_masked(perc):
@@ -116,7 +123,7 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
                         perc = perc.data
                             
                     loc_slice.data = perc
-                    loc_slice.remove_coord("day_of_year")
+#                    loc_slice.remove_coord("day_of_year")
                     
                     list_of_sub_cubes.append(loc_slice)
                     
@@ -132,18 +139,115 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
             
             clim_cube.data = np.ma.masked_equal(clim_cube.core_data(), masked_val)
             
-            self.__logger__.info(clim_cube)
+            incident_data = loc_cube[r]-clim_cube
+            incident_data.data = np.ma.masked_where(incident_data.core_data() <= 0, incident_data.core_data(), copy = True)
             
-            for yx_slice in clim_cube.slices(['latitude', 'longitude']):
-                import iris.quickplot as qplt
+            severity = incident_data * np.atleast_3d(np.array([np.diff(bds) for bds in incident_data.coord("time").bounds]))
+            severity.units = cf_units.Unit(str(severity.units) + 
+                                           " " + 
+                                           str(severity.coord("time").units).split(" ")[0])
+            severity = severity.collapsed("time", iris.analysis.SUM) 
+            severity.long_name = "severity"
+            
+            magnitude = incident_data.collapsed("time", iris.analysis.MAX)
+            magnitude.long_name = "magnitude"
+            
+            duration = (incident_data * 0 + 1) * np.atleast_3d(np.array([np.diff(bds) for bds in incident_data.coord("time").bounds]))
+            duration.units = (str(duration.coord("time").units).split(" ")[0])
+            duration = duration.collapsed("time", iris.analysis.SUM)
+            duration.long_name = "duration"
+            
+            grid_areas = iris.analysis.cartography.area_weights(severity)
+            severity_av = severity.collapsed(["latitude", "longitude"], iris.analysis.MEAN, weights=grid_areas)             
+            magnitude_av = magnitude.collapsed(["latitude", "longitude"], iris.analysis.MEAN, weights=grid_areas) 
+            duration_av = duration.collapsed(["latitude", "longitude"], iris.analysis.MEAN, weights=grid_areas) 
+            
+            
+            def ellipsoidal_distance(lat1, long1, lat2, long2):
+                # https://www.johndcook.com/blog/2018/11/24/spheroid-distance/
+
+                a = 6378137.0 # equatorial radius in meters 
+                f = 1/298.257223563 # ellipsoid flattening 
+                b = (1 - f)*a 
+                tolerance = 1e-11 # to stop iteration
+            
+                phi1, phi2 = lat1, lat2
+                U1 = arctan((1-f)*tan(phi1))
+                U2 = arctan((1-f)*tan(phi2))
+                L1, L2 = long1, long2
+                L = L2 - L1
+            
+                lambda_old = L + 0
+            
+                while True:
                 
-                # Draw the contour with 25 levels.
-                qplt.pcolormesh(yx_slice, vmin=240, vmax=300)
+                    t = (cos(U2)*sin(lambda_old))**2
+                    t += (cos(U1)*sin(U2) - sin(U1)*cos(U2)*cos(lambda_old))**2
+                    sin_sigma = t**0.5
+                    cos_sigma = sin(U1)*sin(U2) + cos(U1)*cos(U2)*cos(lambda_old)
+                    sigma = arctan2(sin_sigma, cos_sigma) 
+                
+                    sin_alpha = cos(U1)*cos(U2)*sin(lambda_old) / sin_sigma
+                    cos_sq_alpha = 1 - sin_alpha**2
+                    if cos_sq_alpha == 0: # for 0 there is no solution (=>nan)
+                        cos_sq_alpha = tolerance/100
+                    cos_2sigma_m = cos_sigma - 2*sin(U1)*sin(U2)/cos_sq_alpha
+                    C = f*cos_sq_alpha*(4 + f*(4-3*cos_sq_alpha))/16
+                
+                    t = sigma + C*sin_sigma*(cos_2sigma_m + C*cos_sigma*(-1 + 2*cos_2sigma_m**2))
+                    lambda_new = L + (1 - C)*f*sin_alpha*t
+                    
+                    if abs(lambda_new - lambda_old) <= tolerance:
+                        break
+                    else:
+                        lambda_old = lambda_new
+            
+                u2 = cos_sq_alpha*((a**2 - b**2)/b**2)
+                A = 1 + (u2/16384)*(4096 + u2*(-768+u2*(320 - 175*u2)))
+                B = (u2/1024)*(256 + u2*(-128 + u2*(74 - 47*u2)))
+                t = cos_2sigma_m + 0.25*B*(cos_sigma*(-1 + 2*cos_2sigma_m**2))
+                t -= (B/6)*cos_2sigma_m*(-3 + 4*sin_sigma**2)*(-3 + 4*cos_2sigma_m**2)
+                delta_sigma = B * sin_sigma * t
+                s = b*A*(sigma - delta_sigma)
+            
+                return s
+            
+            extent = (duration * 0 + 1.).collapsed("longitude", iris.analysis.SUM)
+            
+            pix_sizes_km2 = []
+            for labs in latbnds:
+                lobs = lonbnds[0] # nominally equal for all longitude bounds
+                
+                #approximated pixel side lengths (lat is the same for both sides)
+                e1lon = ellipsoidal_distance(deg2rad(labs[1]), deg2rad(lobs[0]), deg2rad(labs[1]), deg2rad(lobs[1]))
+                e2lon = ellipsoidal_distance(deg2rad(labs[0]), deg2rad(lobs[0]), deg2rad(labs[0]), deg2rad(lobs[1]))
+                e0lat = ellipsoidal_distance(deg2rad(labs[0]), deg2rad(lobs[0]), deg2rad(labs[1]), deg2rad(lobs[0]))
+                
+                pix_size = round(e0lat * (e1lon + e2lon) / 2) #size rounded to m**2 assuming trapezoid
+                
+                pix_sizes_km2.append(pix_size/10e6)
+            
+            extent = (extent * np.array(pix_sizes_km2)).collapsed("latitude", iris.analysis.SUM)
+            extent.units = cf_units.Unit("km2")
+            
+            self.__logger__.info("Extremes table")
+            self.__logger__.info("mean severity: {:.2f} {}".format(severity_av.data, severity_av.units))
+            self.__logger__.info("mean magnitude: {:.2f} {}".format(magnitude_av.data, magnitude_av.units))
+            self.__logger__.info("mean duration: {:.2f} {}".format(duration_av.data, duration_av.units))
+            self.__logger__.info("mean duration: {:.2f} {}".format(extent.data, extent.units))
+            
+            # plotting for trials
+            import iris.quickplot as qplt
+            
+            for dat in ["severity", "magnitude", "duration"]:
+                self.__logger__.info(locals()[dat])
+            
+                qplt.pcolormesh(locals()[dat])
                 
                 # Add coastlines to the map created by contourf.
                 plt.gca().coastlines()
                 
-                plt.savefig(self.__plot_dir__ + os.sep + str(yx_slice.coord("time").points[0]).replace(".","_") + ".png")
+                plt.savefig(self.__plot_dir__ + os.sep + dat + ".png")
                 
                 plt.close()
                 
