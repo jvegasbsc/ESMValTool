afd5e533c405279f6c0cd3680c8f1b604c5b3934
Bas Crezee <bascrezee@gmail.com>
2019-08-07 15:39:06 +0200

    restructured calculation of extremes clim by converting to pandas timeseries

--- esmvaltool/diag_scripts/qualassess/auxiliary/c3s_511_ex.py
+++ esmvaltool/diag_scripts/qualassess/auxiliary/c3s_511_ex.py
@@ -7,6 +7,7 @@ Created on Wed Dec  5 13:59:59 2018
 """
 
 import iris
+import iris.pandas as ipd
 import os
 import sys
 import matplotlib.pyplot as plt
@@ -32,14 +33,14 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
         super(ex_Diagnostic_SP, self).set_info(**kwargs)
         
         # add a region to the regions object
-#        self.__regions__ = dict({
-#            'MAR_region_Jul/Sept': {
-#                'latitude': (-60, 50),
-#                'longitude': (-60, 0),
-#                'time': (datetime.datetime(2000, 7, 1),
-#                         datetime.datetime(2000, 9, 30)
-#                         )
-#                }})
+        self.__regions__ = dict({
+            'CE_JJA_2003': {  # https://en.wikipedia.org/wiki/2003_European_heat_wave
+                'latitude': (43, 47),
+                'longitude': (-2, 4),
+                'time': (datetime.datetime(2003, 7, 20),
+                         datetime.datetime(2003, 8, 20)
+                         )
+                }})
     
     def run_diagnostic(self):
 #        self.sp_data = self.__spatiotemp_subsets__(self.sp_data)['Europe_2000']
@@ -51,8 +52,8 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
         
         this_function =  "extremes example"
         
-        which_percentile = 90
-        window_size = 40 # one directional 5 => 11
+        which_percentile = 10
+        window_size = 5 # one directional 5 => 11
         masked_val = None
         
         # this the extremes example
@@ -81,130 +82,64 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
         
         # Loop over the different regions (i.e. the different events)
         for r,def_r in self.__regions__.items():
+            # Now define the three cubes. Note that now they are really cubes,
+            # not dictionaries that contain cubes.
+            event_cube = self.__spatiotemp_subsets__(cube,{r:def_r})[r]
+
+            spatial_def = def_r.copy()
+            spatial_def.pop("time")
+            clim_cube = self.__spatiotemp_subsets__(cube,{r:spatial_def})[r]
+            ex_cube = event_cube.copy()
+
             # Select this specific region, note that loc_cube is a dictionary with loc_cube[key]
             # holding the actual cubes.
             loc_cube = self.__spatiotemp_subsets__(cube,{r:def_r})
 
-            spatial_def = def_r.copy()
-            spatial_def.pop("time")
-            spatial_loc_cube = self.__spatiotemp_subsets__(cube,{r:spatial_def})
             
             list_of_cubes = []
             
-            #import IPython;IPython.embed()
-            # Slicing and merging cubes sometimes leads to the wrong dimensions
-            # being propagated to coordinates instead of being auxiliary coordinates.
-            # Therefore they are removed. 
-            # TODO: handle this more specifically, in certain cases this might fail.
-            for (entry,ecube) in spatial_loc_cube.items():
-                # Realize the data at this stage
-                ecube.data
-                for rcoord in ["day_of_month", "month_number", "year", self.level_dim]:
-                    if rcoord in [coord.name() for coord in ecube.coords()]:
-                        ecube.remove_coord(rcoord)
-                        loc_cube[r].remove_coord(rcoord)
-            
-
-            n_gridpoints = spatial_loc_cube[r].coord('latitude').shape[0] *\
-                spatial_loc_cube[r].coord('longitude').shape[0]
+            # Now loop over each gridpoint in the selected region
+            counter_gridpoints = 0
+            n_gridpoints = event_cube.shape[1]*event_cube.shape[2]
             self.__logger__.info("Start calculation of extreme climatology \
                                  for %s gridpoints",n_gridpoints)
-            # Now loop over each gridpoint in the selected region
-            for nn,yx_slice in enumerate(spatial_loc_cube[r].slices(['time'])):
-                # Prepare for calculating extremes climatology
-                self.__logger__.info("Progress of xclim: %s percent",np.round(100.*(nn/n_gridpoints),decimals=1))
-                agg_cube = yx_slice.aggregated_by("day_of_year",iris.analysis.MEAN)
-                list_of_sub_cubes=[]
 
-                # Now loop over each doy and extract a temporal window surrounding this doy
-                for doy in np.sort(agg_cube.coord("day_of_year").points):
-                    # loc_slice is created here for carrying on the metadata
-                    self.__logger__.info("#")
-                    loc_slice = agg_cube.extract(iris.Constraint(day_of_year=doy))
-                    tmin = (doy - window_size) % 366
-                    tmax = (doy + window_size) % 366
-                    if not tmin:
-                        tmin = 366
-                    if not tmax:
-                        tmax = 366
-                    if tmin > tmax:
-                        doy_sel = yx_slice.extract(
-                                iris.Constraint(coord_values={'day_of_year':
-                                    lambda cell: 1 <= cell <= tmin or
-                                                 tmax <= cell <= 366}))
-                    else:
-                        doy_sel = yx_slice.extract(
-                                iris.Constraint(coord_values={'day_of_year':
-                                    lambda cell: tmin <= cell <= tmax}))
-
-                    # Do a check if there is actually data
-                    if len(doy_sel.coord("time").points)>1:
-                        perc = doy_sel.collapsed("time",
-                                                 iris.analysis.PERCENTILE,
-                                                 percent=[which_percentile]
-                                                 ).core_data()
-                    else:
-                        perc = doy_sel.core_data()
-
-                    # Catch the case of a masked array
-                    if np.ma.is_masked(perc):
-                        masked_val = perc.mask
-                        perc = perc.data
-                            
-                    # Now the data is inserted back into a cube
-                    loc_slice.data = perc
-                    
-                    list_of_sub_cubes.append(loc_slice)
-                    
-                # TODO: make make more explicit, merge('day_of_year')
-                # t_cube can be interpreted as a timeseries of one pixel 
-
-                t_cube = iris.util.squeeze(iris.cube.CubeList(list_of_sub_cubes).merge()[0])
-                t_cube = cube_sorted(t_cube,"day_of_year")
-                iris.util.promote_aux_coord_to_dim_coord(t_cube, "day_of_year")
-
-                # Since there is no promote_scalar_coord_to_dim_coord, do it like this:
-                iris.util.new_axis(t_cube, "latitude")
-                iris.util.new_axis(t_cube, "longitude")
-                list_of_cubes.append(t_cube)
-
+            for ii in range(event_cube.shape[1]):
+                for jj in range(event_cube.shape[2]):
+                    # Get doy
+                    gridpoint_ts = ipd.as_series(event_cube[:,ii,jj])
+                    doy_to_process = event_cube.coord('day_of_year').points
+                    n_doy = len(doy_to_process)
+                    for n,doy in enumerate(event_cube.coord('day_of_year').points):
+                        doy_window = (doy - window_size < gridpoint_ts.index.dayofyear) &\
+                                     (gridpoint_ts.index.dayofyear < doy + window_size)
+                        perc_val = np.nanpercentile(gridpoint_ts[doy_window],which_percentile)
+                        ex_cube.data[n,ii,jj] = perc_val
+                    self.__logger__.info("Progress of xclim: %s percent",\
+                                         np.round(100.*(counter_gridpoints/n_gridpoints),decimals=1))
+                    counter_gridpoints += 1
+            #TODO think about propagation of nan values
+            ex_cube.data = np.ma.masked_equal(ex_cube.core_data(), np.nan)
             self.__logger__.info("Finished calculation of extreme climatology")
 
-                
-            # Here Iris puts back the list of individual gridpoint timeseries 
-            # on a 'lat lon grid'
-            self.__logger__.info("Start merging of gridpoints back to grid")
-            list_of_cubes_merged = iris.cube.CubeList(list_of_cubes).merge()
-            self.__logger__.info("Finished merging of gridpoints back to grid")
-            assert(len(list_of_cubes_merged)==1)
-            clim_cube = list_of_cubes_merged[0]
+            # Calculate exceedance of the extremes climatology
+            if which_percentile > 50: # we are interested in over threshold values
+                incident_cube = event_cube - ex_cube
+            elif which_percentile < 50: # we are interested in under threshold values
+                incident_cube = ex_cube - event_cube
+            else:
+                self.__loger__.error("Percentile can not be 50, that wouldn't be an extreme climatology")
+                raise ValueError
+            # Note that due to the above check, incident_cube values of the event are always positive
+            # therefore mask negative values
+            incident_cube.data = np.ma.masked_where(incident_cube.core_data() <= 0, incident_cube.core_data(), copy = True)
             
-            clim_cube.data = np.ma.masked_equal(clim_cube.core_data(), masked_val)
-            
-            # This is needed to have the same order and names of dim_coords on both cubes
-            self.__logger__.info("Reordering dimensions")
-            cc_dim_order = [c.name() for c in clim_cube.coords()][:len(clim_cube.shape)]
-            loc_cube_doy = loc_cube[r].copy()
-            iris.util.promote_aux_coord_to_dim_coord(loc_cube_doy, "day_of_year")
-            lc_dim_order = [c.name() for c in loc_cube_doy.coords()][:len(loc_cube_doy.shape)]
-            clim_cube.transpose([cc_dim_order.index(i) for i in lc_dim_order])
-            self.__logger__.info("Finished dimensions")
-
-            # BAS: check issue. At this point, loc_cube[r] still has aux coord day_of_year, even when removing it above
-            # Here the actual exceedance of the extreme climatology is calculated
-            loc_cube_doy.remove_coord("time")
-            clim_cube.remove_coord("time")
-            doy_subset = [clim_cube.coord("day_of_year").points.tolist().index(i) for i in loc_cube_doy.coord("day_of_year").points.tolist()]
-            
-            incident_data = loc_cube_doy-clim_cube[doy_subset,:,:]
-            incident_data.data = np.ma.masked_where(incident_data.core_data() <= 0, incident_data.core_data(), copy = True)
-            
-            # Sidenote: 
-            # TODO: Insert a check that incident_data cube does not have two or more dims with the same shape 
+            # Sidenote:
+            # TODO: Insert a check that incident_cube does not have two or more dims with the same shape
             # otherwise broadcast could fail and produce erroneous results without notice
             
             # calculate severity
-            severity = incident_data * np.atleast_3d(np.array([np.diff(bds) for bds in loc_cube[r].coord("time").bounds]))
+            severity = incident_cube * np.atleast_3d(np.array([np.diff(bds) for bds in loc_cube[r].coord("time").bounds]))
             severity.units = cf_units.Unit(str(cube.units) + 
                                            " " + 
                                            str(cube.coord("time").units).split(" ")[0])
@@ -212,17 +147,19 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
             severity.long_name = "severity"
             
             # calculate magnitude
-            magnitude = incident_data.collapsed("day_of_year", iris.analysis.MAX)
+            magnitude = incident_cube.collapsed("day_of_year", iris.analysis.MAX)
             magnitude.long_name = "magnitude"
             
             # calculate duration
-            duration = (incident_data * 0 + 1) * np.atleast_3d(np.array([np.diff(bds) for bds in loc_cube[r].coord("time").bounds]))
+            duration = (incident_cube * 0 + 1) * np.atleast_3d(np.array([np.diff(bds) for bds in loc_cube[r].coord("time").bounds]))
             duration.units = (str(cube.coord("time").units).split(" ")[0])
             duration = duration.collapsed("day_of_year", iris.analysis.SUM)
             duration.long_name = "duration"
             
             # calculate spatial averages
             grid_areas = iris.analysis.cartography.area_weights(severity)
+            #TODO: below three lines should be changed to 
+            # a nanmean (i.e. taking the mean over the not-nan values)
             severity_av = severity.collapsed(["latitude", "longitude"], iris.analysis.MEAN, weights=grid_areas)             
             magnitude_av = magnitude.collapsed(["latitude", "longitude"], iris.analysis.MEAN, weights=grid_areas) 
             duration_av = duration.collapsed(["latitude", "longitude"], iris.analysis.MEAN, weights=grid_areas) 
@@ -238,22 +175,23 @@ class ex_Diagnostic_SP(Basic_Diagnostic_SP):
             self.__logger__.info("mean magnitude: {:.2f} {}".format(magnitude_av.data, magnitude_av.units))
             self.__logger__.info("mean duration: {:.2f} {}".format(duration_av.data, duration_av.units))
             self.__logger__.info("extent: {:.2f} {}".format(extent.data, extent.units))
-            
             # plotting for trials
             import iris.quickplot as qplt
             
             for dat in ["severity", "magnitude", "duration"]:
-                self.__logger__.info(locals()[dat])
+                #self.__logger__.info(locals()[dat])
             
                 qplt.pcolormesh(locals()[dat])#, vmin = 250, vmax = 300)
                 
                 # Add coastlines to the map created by contourf.
-                plt.gca().coastlines()
+                plt.gca().coastlines("10m")
                 plt.colorbar()
                 plt.savefig(self.__plot_dir__ + os.sep + dat + ".png")
                 
                 plt.close()
-                
+
+            import IPython;IPython.embed()
+
         # produce report
         expected_input, found = \
             self.__file_anouncement__(subdir="c3s_511/single_extremes_input",
